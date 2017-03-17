1298961bf8262025bbcbae31dfa767b2881b2b68
Chris Marlow <christopher.marlow@shopify.com>
2017-03-16 20:32:20 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import './styles.css'
+import RX from 'rx'
+
+const canvas = document.getElementsByTagName('canvas')[0]
+
+const drawDot = ([x,y]) => canvas.getContext('2d').fillRect(x, y, 1, 1)
+const log = console.log.bind(log)
+
+const coordinates = Rx.Observable
+                      .fromEvent(canvas, 'mousemove')
+                      .map( ({clientX, clientY}) => [clientX, clientY] )
+
+coordinates.forEach(drawDot)
+coordinates.forEach(log)
+coordinates.forEach(([x,y]) => {
+  console.log(coordinates.toArray())
+})
+
