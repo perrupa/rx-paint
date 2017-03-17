6f62b7e888d3b52c60fbc62d8a0c36a18276545e
Chris Marlow <christopher.marlow@shopify.com>
2017-03-16 21:00:54 -0400

    draw on drag

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const canvas = document.getElementsByTagName('canvas')[0]
 const drawDot = ([x,y]) => canvas.getContext('2d').fillRect(x, y, 1, 1)
 const log = console.log.bind(log)
 
-const coordinates = Rx.Observable
-                      .fromEvent(canvas, 'mousemove')
+const mouseDown   = Rx.Observable.fromEvent(document, "mousedown")
+const mouseUp     = Rx.Observable.fromEvent(document, "mouseup")
+const coordinates = Rx.Observable.fromEvent(canvas, 'mousemove')
                       .map( ({clientX, clientY}) => [clientX, clientY] )
 
-coordinates.forEach(drawDot)
-coordinates.forEach(log)
+const drawing = mouseDown.flatMap(() => coordinates.takeUntil(mouseUp))
 
+drawing.forEach(drawDot)
